f18d1cccdf1caeb15494a6529bb62ce6a42c0665
Gustavo Okamoto <gustavookamoto1945@gmail.com>
2023-02-15 16:33:09 -0300

    Criando o projeto

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,31 @@
+import '../../assets/css/navBar.scss'
+import { Link } from "react-router-dom";
+import sp from '../../assets/images/sp.png'
+
+
+function NavBar() {
+  return (
+   <nav>
+    <div id='title-logo'>
+      <img id='logo' src={sp}></img>
+      <h2>Pirituba</h2>
+    </div>
+    <div id='links-div'>
+        <button className='link-button'>
+          <Link to="/" className='nav-link'>Pagina Principal</Link>
+        </button>
+        <button className='link-button'>
+          <Link to="/demografia" className='nav-link'>Demografia</Link>
+        </button>
+        <button className='link-button'>
+          <Link to="/" className='nav-link'>Comércio Local</Link>
+        </button>
+        <button className='link-button'>
+          <Link to="/" className='nav-link'>Curiosidades</Link>
+        </button>
+      </div>
+   </nav>
+  );
+}
+
+export default NavBar;
